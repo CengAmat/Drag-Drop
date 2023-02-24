ad770351c1b687e2f776069b32b13c03dcfee7ad
cengamat <azurnaci@cultwinesltd.com>
2023-02-24 16:18:31 +0000

    Understanding various Import & Export Syntaxes

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,10 +1,10 @@
-import { Component } from "./base-component.js";
-import { Validatable, validate } from "../util/validation.js";
+import Cmp from "./base-component.js";
+import * as Validation from "../util/validation.js";
 import { Autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
 
 // ProjectInput Class
-export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement>{
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
@@ -28,25 +28,25 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        const titleValidatable: Validatable = {
+        const titleValidatable: Validation.Validatable = {
             value: enteredTitle,
             required: true
         }
-        const descriptionValidatable: Validatable = {
+        const descriptionValidatable: Validation.Validatable = {
             value: enteredDescription,
             required: true,
             minLength: 5
         }
-        const peoppleValidatable: Validatable = {
+        const peoppleValidatable: Validation.Validatable = {
             value: enteredPeople,
             required: true,
             min: 1,
             max: 5
         }
 
-        if (!validate(titleValidatable) ||
-            !validate(descriptionValidatable) ||
-            !validate(peoppleValidatable)) {
+        if (!Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(peoppleValidatable)) {
             alert('Invalid input, please try again!');
             return;
         } else {
